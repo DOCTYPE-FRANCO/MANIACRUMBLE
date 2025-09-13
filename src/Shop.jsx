a052b788c8440c20db49e8652bb91e3cfd08cdc1
Franco <ufrancis186@gmail.com>
2025-09-13 20:14:03 +0100

    more additions

---
+++ src/Shop.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+function Shop(){
+    const products = [
+        {
+            id: 1,
+            name: "Beanie",
+            description: "A stylish black beanie.",
+            price: 19.99,
+            image: "path/to/beanie.jpg"
+        },
+        {
+            id: 2,
+            name: "Wave Cap",
+            description: "A trendy black wave cap.",
+            price: 24.99,
+            image: "path/to/wavecap.jpg"
+        }
+    ]
+
+    return(
+        <div></div>
+    );
+}
+
+export default Shop;
