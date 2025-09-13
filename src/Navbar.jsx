bcb34f5008fd308e65600481574cb8e1f202fca2
Franco <ufrancis186@gmail.com>
2025-09-13 20:26:41 +0100

    fix again

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar(){
         <div className="flex justify-center items-center ">
             <ul className="md:flex gap-10 hidden">
                 <li className="text-white font-bold hover:text-gray-600"><Link>Home</Link></li>
-                <li className="text-white font-bold hover:text-gray-600"><Link>Shop</Link></li>
+                <li className="text-white font-bold hover:text-gray-600"><Link to="/shop">Shop</Link></li>
                 <li className="text-white font-bold hover:text-gray-600"><Link>Contact</Link></li>
             </ul>
         </div>
