ee430500939d807d32528c612b5516da59c5e1f6
Franco <ufrancis186@gmail.com>
2025-12-11 23:04:13 +0100

    lil fix to herosection

--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -6,7 +6,7 @@ import {motion} from "framer-motion";
 function HeroSection() {
     return (
         <div className="flex flex-col md:flex-row md:gap-[150px] gap-10 justify-center items-center mt-32 md:mt-2">
-            <div className="flex flex-col justify-center gap-4  items-center">
+            <div className="flex flex-col justify-center gap-4  items-center hover:scale-105 transition-transform duration-500">
                 <p className="text-4xl md:text-5xl text-gray-500 font-bold ">
                     <Typewriter 
                         options={{
@@ -18,7 +18,7 @@ function HeroSection() {
                 </p>
                 <p className=" text-4xl md:text-6xl text-white font-extrabold">MANIACRUMBLE</p>
             </div>
-            <div className="w-[300px] justify-center pt-10">
+            <div className="w-[300px] justify-center pt-10 hover:scale-105 transition-transform duration-500">
                 <motion.img
                     src={HeroImg}
                     className="w-[700px] rounded-r-full"
