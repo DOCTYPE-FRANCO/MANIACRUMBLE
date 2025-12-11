400e72176f9a9a7eb4ec35d7dd333fa6cf9253c5
Franco <ufrancis186@gmail.com>
2025-12-11 22:04:20 +0100

    lil update

--- src/App.jsx
+++ src/App.jsx
@@ -16,18 +16,7 @@ function App() {
       <CartProvider>
 
         <div className="min-h-screen w-full bg-black relative">
-            {/* Dark White Dotted Grid Background */}
-            <div
-              className="absolute inset-0 z-0"
-              style={{
-                background: "#000000",
-                backgroundImage: `
-                  radial-gradient(circle, rgba(255, 255, 255, 0.2) 1.5px, transparent 1.5px)
-                `,
-                backgroundSize: "30px 30px",
-                backgroundPosition: "0 0",
-              }}
-            />
+           
               {/* Your Content/Components */}
 
               <Header />
--- src/HeroSection.jsx
+++ src/HeroSection.jsx
@@ -5,12 +5,12 @@ import {motion} from "framer-motion";
 
 function HeroSection() {
     return (
-        <div className="flex flex-col md:flex-row md:gap-[150px] gap-10 justify-center items-center mt-24 md:mt-2">
-            <div className="flex flex-col justify-center gap-4">
+        <div className="flex flex-col md:flex-row md:gap-[150px] gap-10 justify-center items-center mt-32 md:mt-2">
+            <div className="flex flex-col justify-center gap-4 w-full items-center">
                 <p className="text-4xl md:text-5xl text-gray-500 font-bold ">
                     <Typewriter 
                         options={{
-                            strings: ["Don't think. Jump !"],
+                            strings: ["Don't think. Jump!"],
                             autoStart: true,
                             loop: true,
                         }}
